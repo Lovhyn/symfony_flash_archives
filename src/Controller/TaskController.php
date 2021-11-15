b6a21af22c6abb305b1dc592722fcba7130d6d76
François Demestre <fdemestre@laposte.net>
2021-11-15 14:05:53 +0100

    Récupération des entrées avec le var dumper dd()

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Entity\Tasks;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class TaskController extends AbstractController
+{
+    /**
+     * @Route("/task/listing", name="task")
+     */
+    public function index(): Response
+    {
+
+        //  On va chercher avec doctrine le repository de nos tâches
+        $repository = $this->getDoctrine()->getRepository(Tasks::class);
+        //  Danc ce repository, nous récupérons toutes les entrées
+        $tasks = $repository->findAll();
+        //  Affichage des données dans le var-dumper
+
+        dd($tasks);
+
+
+
+        return $this->render('task/index.html.twig', [
+            'controller_name' => 'TaskController',
+        ]);
+    }
+}
