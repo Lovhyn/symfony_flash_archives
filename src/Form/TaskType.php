11651077cec14b6b811ddef5fe22e829d0862e56
Manaaia <85735324+Manaaia@users.noreply.github.com>
2021-11-24 16:03:14 +0100

    Ajout API

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use Bartender;
 use App\Entity\Tag;
 use App\Entity\Tasks;
 use Doctrine\ORM\EntityRepository;
@@ -10,6 +11,7 @@ use Doctrine\DBAL\Types\BooleanType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -32,8 +34,13 @@ class TaskType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+
+        $bartender = new Bartender();
+        $filteredBeerListNameName = $bartender->filterBeerList();
+
         $builder
-            ->add('name', TextType::class, [
+            ->add('name', ChoiceType::class, [
+                'choices' => $filteredBeerListNameName,
                 'label' => $this->translator->trans('general.name')
             ])
             ->add('description', TextareaType::class, [
